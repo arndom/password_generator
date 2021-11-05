14b57ce2e758e82988b718c2f5d85592e1b8aa5b
arndom <aminonimisi2@gmail.com>
2021-11-05 21:47:42 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Autorenew, ContentCopy, GitHub } from "@mui/icons-material";
 import { TextField } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 import Toggle from "./components/Toggle";
 import { keepTheme, Password } from "./utils/utils";
@@ -58,6 +59,20 @@ const useStyles = makeStyles({
     cursor: "pointer",
 
     "&:hover":{
+      background: "var(--hover-button)"
+    }
+  },
+
+  svgRingGenerate:{
+    width: "44px",
+    height: "44px",
+    display:"grid",
+    placeItems: "center",
+    border: "2px solid",
+    borderRadius: "50px",
+    cursor: "pointer",
+
+    "&:hover":{
       background: "var(--generate-button)"
     }
   },
@@ -96,44 +111,36 @@ function App() {
 
   const generatePassword = () => setPassword(Password.generate(16))
   const openLink = () => window.open('https://github.com/arndom', '_blank').focus()
-  const copyText =() => navigator.clipboard.writeText(password)
+  const copyText =() => {
+    navigator.clipboard.writeText(password)
+    toast.success("Copied!")
+  }
 
   return (
     <div className="app">
+      
+      <Toaster position="top-center" reverseOrder={true}/>
+
         <div className={classes.nav}>
-          
           <p className={classes.logo}>Genera+8-Pa$$word</p>
           <div className={classes.navButtons}>
-            <div
-              className={classes.svgRing}
-              onClick= {openLink}
-            >
+            <div className={classes.svgRing} onClick= {openLink}>
               <GitHub/>
             </div>
             <Toggle/>
           </div>
-        
         </div>
 
         <div className={classes.holder}>
-
-          <div 
-            className ={classes.svgRing}
-            onClick = {generatePassword} 
-          >
+          <div className ={classes.svgRingGenerate} onClick = {generatePassword}>
             <Autorenew/>
           </div>
-          
           <div className={classes.navButtons}>
             <TextField disabled id="outlined-basic" variant="outlined" value= {password} className={classes.textField}/>  
-            <div 
-              className = {classes.svgRing}
-              onClick = {copyText}
-            >
+            <div className = {classes.svgRing} onClick = {copyText}>
               <ContentCopy/>
             </div>
           </div>
-
         </div>
 
     </div>
